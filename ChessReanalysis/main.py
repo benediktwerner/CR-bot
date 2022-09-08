2388f85db5ea175500df9ad3ac4956fa3b5b55d7
Benedikt Werner <1benediktwerner@gmail.com>
2022-09-08 13:40:21 +0200

    Make CR accept 960

--- ChessReanalysis/main.py
+++ ChessReanalysis/main.py
@@ -30,7 +30,11 @@ def load_pgn(filename):
             if not game:
                 break
             gid = gameid(game)
-            if gid and game.headers.get("Variant") in ("Standard", "From Position"):
+            if gid and game.headers.get("Variant") in (
+                "Standard",
+                "From Position",
+                "Chess960",
+            ):
                 working_set[gid] = game
             n += 1
         print(f"Added {n} games to working set from {filename}")
